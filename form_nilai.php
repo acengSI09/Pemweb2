89ddd8872a96fd06df379ddfa68fd8a8eaa3c8f1
acengSI09 <146155892+acengSI09@users.noreply.github.com>
2024-02-28 11:15:45 +0700

    INI PRAKTIKUM WEEK 2

---
+++ form_nilai.php
@@ -0,0 +1,45 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<form method="GET" action="hasil_nilai.php">
+    <h1>Form Nilai Siswa</h1>
+  <div class="form-group row">
+    <label for="text" class="col-4 col-form-label">Nama Lengkap</label> 
+    <div class="col-8">
+      <input id="text" name="nama" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="select" class="col-4 col-form-label">Mata Kuliah</label> 
+    <div class="col-8">
+      <select id="select" name="matkul" class="custom-select">
+        <option value="ddp">Dasar Dasar Pemrograman</option>
+        <option value="pw">Pemrogramn Web</option>
+        <option value="bd">Basis Data</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nilai_uts" class="col-4 col-form-label">Nilai UTS</label> 
+    <div class="col-8">
+      <input id="nilai_uts" name="nilai_uts" placeholder="Nilai UTS" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nilai_uas" class="col-4 col-form-label">Nilai UAS</label> 
+    <div class="col-8">
+      <input id="nilai_uas" name="nilai_uas" placeholder="Nilai UAS" type="text" class="form-control">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="nilai_tugas" class="col-4 col-form-label">Nilai Tugas</label> 
+    <div class="col-8">
+      <input id="nilai_tugas" name="nilai_tugas" placeholder="Nilai Tugas" type="text" class="form-control">
+    </div>
+  </div> 
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
