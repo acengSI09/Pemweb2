71d7fff77c08f55739ee456560525a0469990ffa
acengSI09 <146155892+acengSI09@users.noreply.github.com>
2024-03-10 18:57:02 +0700

    INI TUGAS WEEK 3 FORM REGISTRASI

---
+++ home_pendaftaran_siswa.php
@@ -0,0 +1,150 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"> 
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<h1>Form Pendaftaran Siswa</h1>
+<form method="POST" action="#">
+    <br>
+  <div class="form-group row">
+    <label for="nama" class="col-4 col-form-label">Nama</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-address-card"></i>
+          </div>
+        </div> 
+        <input id="nama" name="nama" type="text" class="form-control">
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Jeni Kelamin</label> 
+    <div class="col-8">
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="jk" id="jk_0" type="radio" class="custom-control-input" value="laki-laki"> 
+        <label for="jk_0" class="custom-control-label">Laki-laki</label>
+      </div>
+      <div class="custom-control custom-radio custom-control-inline">
+        <input name="jk" id="jk_1" type="radio" class="custom-control-input" value="perempuan"> 
+        <label for="jk_1" class="custom-control-label">Perempuan</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="prodi" class="col-4 col-form-label">Program Studi</label> 
+    <div class="col-8">
+      <select id="prodi" name="prodi" class="custom-select">
+        <option value="Sistem Informasi">Sistem Informasi</option>
+        <option value="Teknik Informatika">Teknik Informatika</option>
+        <option value="Bisnis Digital">Bisnis Digital</option>
+      </select>
+    </div>
+  </div> 
+  </div>
+  <div class="form-group row">
+    <label class="col-4">Hobi</label> 
+    <div class="col-8">
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="hobi[]" id="hobi_0" type="checkbox" class="custom-control-input" value="html"> 
+        <label for="hobi_0" class="custom-control-label">HTML</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="hobi[]" id="hobi_1" type="checkbox" class="custom-control-input" value="css"> 
+        <label for="hobi_1" class="custom-control-label">CSS</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="hobi[]" id="hobi_2" type="checkbox" class="custom-control-input" value="rwd"> 
+        <label for="hobi_2" class="custom-control-label">RWD Bootsrap</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="hobi[]" id="hobi_3" type="checkbox" class="custom-control-input" value="php"> 
+        <label for="hobi_3" class="custom-control-label">PHP</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="hobi[]" id="hobi_4" type="checkbox" class="custom-control-input" value="python"> 
+        <label for="hobi_4" class="custom-control-label">Python</label>
+      </div>
+      <div class="custom-control custom-checkbox custom-control-inline">
+        <input name="hobi[]" id="hobi_5" type="checkbox" class="custom-control-input" value="java"> 
+        <label for="hobi_5" class="custom-control-label">Java</label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="prodi" class="col-4 col-form-label">Tempat Domisili</label> 
+    <div class="col-8">
+      <select id="domisili" name="domisili" class="custom-select">
+        <option value="Jakarta">Jakarta</option>
+        <option value="Depok">Depok</option>
+        <option value="Bogor">Bogor</option>
+      </select>
+    </div> 
+  </div>
+  <div class="form-group row">
+    <label for="ipk" class="col-4 col-form-label">IPK</label> 
+    <div class="col-8">
+      <input id="ipk" name="ipk" type="text" class="form-control">
+    </div>
+  </div>
+  </div>
+  <div class="form-group row">
+    <label for="email" class="col-4 col-form-label">Email</label> 
+    <div class="col-8">
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <div class="input-group-text">
+            <i class="fa fa-envelope"></i>
+          </div>
+        </div> 
+        <input id="email" name="email" type="text" class="form-control">
+      </div>
+    </div>
+  </div>  
+  <div class="form-group row">
+    <div class="offset-4 col-8">
+      <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+    </div>
+  </div>
+</form>
+
+<?php
+$nama = $_POST['nama'];
+$jk = $_POST['jk'];
+$prodi = $_POST['prodi'];
+$hobi = $_POST['hobi'];
+$domisili = $_POST['domisili'];
+$ipk = $_POST['ipk'];
+$email = $_POST['email'];
+
+//print hobi
+
+
+//Mencari status LULUS/TIDAK LULUS
+
+function status($ipk){
+    if ($ipk>=3 && $ipk<=4){
+        return "LULUS";
+    }
+    else {
+        return "TIDAK LULUS";
+    }
+}
+$status = status($ipk);
+
+//cetah
+echo "<h2>Informasi yang Anda Kirim</h2>";
+echo "Nama: $nama <br>";
+echo "Prodi: $prodi <br>";
+echo "Jenis Kelamin: $jk <br>";
+echo "Hobi: ";
+foreach ($hobi as $value){
+    echo "$value ,";
+}
+echo "<br>";
+echo "Tempat Domisili: $domisili <br>";
+echo "IPK: $ipk <br>";
+echo "Email Anda: $email <br>";
+echo "<br>";
+echo "Status: $status <br>";
+
+?>
